42529297f3ee294cc23ffe88f9918d78f563b825
Kanan Rahimov <mail@kenanbek.me>
2019-09-08 00:32:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     author="LeoHsiao",
     author_email="leohsiao@foxmail.com",
     description="Read and modify metadata of digital image, including EXIF, IPTC, XMP.",
-    long_description=long_description,
+    long_description="See README",
     long_description_content_type="text/markdown",
     url="https://github.com/LeoHsiao1/pyexiv2",
     packages=setuptools.find_packages(),
